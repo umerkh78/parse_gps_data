705911768047fbdd0e40af11dc9aeb518c9a1edb
MuhammadUmerKhan <umerkh378@gmail.com>
2023-07-02 16:06:56 +0500

    Added a struct to save individual parameters of input NMEA string

--- components/parse_gps_data/include/parse_gps_data.h
+++ components/parse_gps_data/include/parse_gps_data.h
@@ -6,6 +6,26 @@ extern "C" {
 
 void parse_gps_data(char arr[]);
 
+//Struct to save parameters of NMEA Message
+typedef struct {
+    char* sat_type;
+    char* timestamp;
+    char* latitude;
+    char* lat_dir;
+    char* longitude;
+    char* long_dir;
+    char* precision;
+    char* sat_no;
+    char* hdp;
+    char* alt;
+    char* unit1;
+    char* geo_sep;
+    char* unit2;
+    char* aoc;
+    char* cor_stat_id;
+    char* checksum;
+} gps_t;
+
 #ifdef __cplusplus
 }
 #endif
