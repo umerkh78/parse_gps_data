f9d861635b85c67fbc2ab9e1f74af3a80e1c8b38
MuhammadUmerKhan <umerkh378@gmail.com>
2023-07-02 15:50:34 +0500

    Added gga_check to verify correctness of input string

--- components/parse_gps_data/parse_gps_data.c
+++ components/parse_gps_data/parse_gps_data.c
@@ -5,5 +5,29 @@ void parse_gps_data(char arr[]);
 
 void parse_gps_data(char arr[])
 {
-    
+
+}
+
+
+static bool gga_check(char ary[])
+{
+    /*This function checks the GGA parameter to check if the right
+    inout string has been entered. GGA is the first parameter of 
+    NMEA format GPS data.*/
+	if (ary[0] == ',') {
+        //Checks if the first parameter is empty
+		return false;
+	}
+	else {
+		size_t sz = strlen(ary);                   //get input array size
+		char dum_ary[sz];                           //create an array of input array size
+		strcpy(dum_ary,ary);                        //copy contents into the dummy array
+
+		char* value = strtok(dum_ary,",");          //tokenize the string to get first parameter
+		char chcker[] = "$GPGGA";
+
+		return !(strcmp(value,chcker)); 
+            /*Compares the value of first parameter with $GPGGA and returns true if 
+            it matches(correct string) and false if it doesn't(incorrect string)*/
+	}
 }
