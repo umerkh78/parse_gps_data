5f9cff524d9d821cc46c879266801ab8a51d734a
MuhammadUmerKhan <umerkh378@gmail.com>
2023-07-02 15:57:30 +0500

    Added checksum function to verify checksum and validate packet integrity

--- components/parse_gps_data/parse_gps_data.c
+++ components/parse_gps_data/parse_gps_data.c
@@ -2,6 +2,7 @@
 #include "parse_gps_data.h"
 
 void parse_gps_data(char arr[]);
+static bool checksum(char array[]);
 
 void parse_gps_data(char arr[])
 {
@@ -30,4 +31,26 @@ static bool gga_check(char ary[])
             /*Compares the value of first parameter with $GPGGA and returns true if 
             it matches(correct string) and false if it doesn't(incorrect string)*/
 	}
+}
+
+static bool checksum(char array[])
+{
+    /*This function verifies checksum of the given data by comparing it with checksum
+    present at the end of NMEA format. Checksum is calculated by doing XOR of all the 
+    characters between the $ and * of the NMEA format GPS data*/
+	int x = 1;
+	int chcksum = 0;
+
+	while (array[x] != '*') {
+        /*This calculates XOR of data between $ and * of the NMEA format GPS data*/
+
+		chcksum ^= array[x];
+		++x;
+	}
+	++x;
+
+	int receivedChecksum = 0;
+	sscanf(&array[x], "%2X", &receivedChecksum);        //Reading and storing the checksum from NMEA in HEX Format
+
+	return receivedChecksum==chcksum;                   //Compare the two and return the boolean
 }
